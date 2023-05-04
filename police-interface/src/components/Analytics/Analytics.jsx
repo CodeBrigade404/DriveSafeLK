5bd5244b42101a6f227e24c5ac12f82d31516365
ManulaK <it21019968@my.sliit.lk>
2023-05-04 09:48:09 +0530

    #9 Feature/Analytics added

--- police-interface/src/components/Analytics/Analytics.jsx
+++ police-interface/src/components/Analytics/Analytics.jsx
@@ -1,11 +1,76 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+import "./Analytics.css";
 
-export default function Analytic() {
+const data = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  datasets: [
+    {
+      label: "Number of Crimes",
+      data: [150, 200, 180, 250, 210, 300, 280, 320, 350, 400, 380, 450],
+      borderColor: "rgba(54, 162, 235, 1)",
+      backgroundColor: "rgba(54, 162, 235, 0.2)",
+      tension: 0.4,
+      fill: true,
+    },
+  ],
+};
+
+const Analytics = () => {
+  const chartRef = useRef(null);
+
+
+useEffect(() => {
+  let chartInstance = null;
+  if (chartRef && chartRef.current) {
+    const ctx = chartRef.current.getContext("2d");
+    if (chartInstance) {
+      chartInstance.destroy();
+    }
+    chartInstance = new Chart(ctx, {
+      type: "line",
+      data: data,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+          },
+        },
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: "Month",
+            },
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: "Number of Crimes",
+            },
+            suggestedMin: 0,
+            suggestedMax: 500,
+          },
+        },
+      },
+    });
+  }
+  return () => {
+    if (chartInstance) {
+      chartInstance.destroy();
+    }
+  };
+}, [chartRef]);
+
   return (
-    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <Typography variant='h4'>Analytics</Typography>
-    </Box>
+    <div className="crime-line-graph-container">
+      <canvas ref={chartRef} />
+    </div>
   );
-}
+};
+
+export default Analytics;
