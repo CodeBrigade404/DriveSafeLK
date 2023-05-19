bb12eb051c90a3c6ef95a9391bfd996394ef9306
Chathuka Viduranga <Chathuka.viduranga.bc@gmail.com>
2023-05-19 20:02:50 +0530

    updated emailtemp

--- police-auth-service/utils/emailTemplate.js
+++ police-auth-service/utils/emailTemplate.js
@@ -1,14 +1,16 @@
 let resetPassword = (email, token) => {
-    const emailTemplate = {
-        from: "noreply@gmail.com",
-        to: email,
-        subject: "Password Reset for" + email,
-        text:
-            "Please click the link below to reset your password \n\n" +
-            "http://localhost:3000/resetPassword/" + token + "\n\n" +
-            "If you did not request this, please ignore this email and your password will remain unchanged.\n",
-    };
-    return emailTemplate;
+  const emailTemplate = {
+    from: 'noreply@gmail.com',
+    to: email,
+    subject: 'Password Reset for' + email,
+    text:
+      'Please click the link below to reset your password \n\n' +
+      'http://drive-safe-lk-police.vercel.app/resetPassword/' +
+      token +
+      '\n\n' +
+      'If you did not request this, please ignore this email and your password will remain unchanged.\n',
+  };
+  return emailTemplate;
 };
 
 export { resetPassword };
