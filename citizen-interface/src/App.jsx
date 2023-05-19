54fb72029e6edd116ad9a804b4fcf3847c12bb7e
Chathuka Viduranga <Chathuka.viduranga.bc@gmail.com>
2023-05-20 03:29:32 +0530

    chngaed

--- citizen-interface/src/App.jsx
+++ citizen-interface/src/App.jsx
@@ -1,30 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Homepage';
-import Login from './Components/CitizenLogin/CitizenLogin.jsx';
+import Login from './Components/CitizenLogin';
 import ForgetPassword from './Components/ForgetPassword';
 import ResetPassword from './Components/ResetPassword';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/login',
-      element: <Login />,
-    },
-    {
-      path: '/forgetPassword',
-      element: <ForgetPassword />,
-    },
-    {
-      path: '/resetPassword/:token',
-      element: <ResetPassword />,
-    },
-  ]);
-  return <RouterProvider router={router} />;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+
+          <Route path='/login' element={<Login />} />
+
+          <Route path='/forgetPassword' element={<ForgetPassword />} />
+
+          <Route path='/resetPassword/:token' element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
